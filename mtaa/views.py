3fa9f640d26eb88ead3211d1b3312a23acb16b28
kebasoh <ongatikebasoba@gmail.com>
2020-02-23 16:08:35 +0300

    styled my navbar

--- mtaa/views.py
+++ mtaa/views.py
@@ -30,7 +30,6 @@ def profile(request):
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            # message.success(request, f'Your account has been updated')
             return render(request,'registration/profile.html')
     else:
         u_form = UserUpdateForm(instance=request.user)
